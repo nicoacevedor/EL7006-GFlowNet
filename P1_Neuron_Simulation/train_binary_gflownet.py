cc2cab37da1ec2004fc0b063556c872ca389b45a
nicoacevedor <nico.acevedo.rivas@gmail.com>
2024-09-27 01:36:14 -0300

    Entrenamiento desde un punto anterior

--- P1_Neuron_Simulation/train_binary_gflownet.py
+++ P1_Neuron_Simulation/train_binary_gflownet.py
@@ -1,4 +1,4 @@
-import torch.cuda as cuda
+import torch
 
 from src.binary_engine import GFlowBinaryEngine
 from src.dataset import NeuronDataset
@@ -9,11 +9,12 @@ from src.simulators import NetworkSystemSimulator
 def main() -> None:
     n_neurons = 3
     model = FlowModel(n_neurons=n_neurons, num_hid=512)
-    simulator = NetworkSystemSimulator(device=("cuda" if cuda.is_available() else "cpu"))
+    model.load_state_dict(torch.load(r"training\binary\exp2\model.pt", weights_only=True))
+    simulator = NetworkSystemSimulator(device=("cuda" if torch.cuda.is_available() else "cpu"))
     engine = GFlowBinaryEngine(model, simulator)
     train_data = NeuronDataset("Id_3x3.pt", map_location=engine.device)
 
-    engine.fit(train_data, n_epochs=10, batch_size=50)
+    engine.fit(train_data, n_epochs=1, batch_size=50)
 
 
 if __name__ == "__main__":
