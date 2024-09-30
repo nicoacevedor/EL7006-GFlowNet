438ab970f0a9437e1f17376d14dacbd772f49fcf
nicoacevedor <nico.acevedo.rivas@gmail.com>
2024-09-30 00:10:54 -0300

    Modificación del modelo par devolver 9 salidas en lugar de 2

--- P1_Neuron_Simulation/src/model.py
+++ P1_Neuron_Simulation/src/model.py
@@ -1,10 +1,11 @@
 import torch.cuda as cuda
 import torch.nn as nn
+from torch import Tensor
 from torch.nn.functional import sigmoid
 
 
 class FlowModel(nn.Module):
-    def __init__(self, n_neurons: int, num_hid: int):
+    def __init__(self, n_neurons: int, num_hid: int) -> None:
         super().__init__()
         self.device = "cuda" if cuda.is_available() else "cpu"
         self.n_neurons = n_neurons
@@ -12,9 +13,10 @@ class FlowModel(nn.Module):
         self.mlp = nn.Sequential(
             nn.Linear(n_neurons * n_neurons, num_hid),
             nn.LeakyReLU(),
-            nn.Linear(num_hid, 2),
+            nn.Linear(num_hid, n_neurons * n_neurons),
         )
 
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
         y = self.mlp(x)
-        return sigmoid(y)
+        # return sigmoid(y)
+        return y.sigmoid() * (1 - x)
