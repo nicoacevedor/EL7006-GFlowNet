d29983c66276688b9333c6903d53088c8869f2f4
nicoacevedor <nacevedo@meteodata.cl>
2024-09-30 19:15:28 -0300

    Nueva función de padres binarios

--- P1_Neuron_Simulation/src/gflownet_functions.py
+++ P1_Neuron_Simulation/src/gflownet_functions.py
@@ -18,10 +18,27 @@ def get_parents_flow_continuous(state: torch.Tensor, step: int) -> torch.Tensor:
     return state[step].sum()
 
 
-def get_parents_flow_binary(
-    parents: list[torch.Tensor], flow_function: torch.nn.Module
-) -> torch.Tensor:
-    return sum(
-        (flow_function(parent).sum() for parent in parents),
-        start=torch.tensor(0.0, device=flow_function.device),
-    )
+# def get_parents_flow_binary(
+#     parents: list[torch.Tensor], flow_function: torch.nn.Module
+# ) -> torch.Tensor:
+#     return sum(
+#         (flow_function(parent).sum() for parent in parents),
+#         start=torch.tensor(0.0, device=flow_function.device),
+#     )
+
+
+def get_parents_flow_binary(state: torch.Tensor, flow_function: torch.nn.Module) -> list[torch.Tensor]:
+    memo = dict()
+    def get_parents_flow_recursive(state: torch.Tensor, memo: dict, flow_function: torch.nn.Module) -> torch.Tensor:
+        indexes = state.nonzero(as_tuple=True)[0]
+        state_flow = torch.tensor(0.)
+        for i in indexes:
+            parent = state.clone()
+            parent[i] = 0
+            if parent not in memo:
+                memo[parent] = flow_function(parent)                        
+            flow = memo[parent][i] + get_parents_flow_recursive(parent, memo, flow_function)
+            state_flow = state_flow + flow
+        return state_flow
+    return get_parents_flow_recursive(state, memo, flow_function)
+            
