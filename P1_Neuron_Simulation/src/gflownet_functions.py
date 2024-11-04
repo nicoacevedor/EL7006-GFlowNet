de09d1f9bf3c1044b1084bd59fb82c139285478b
nicoacevedor <nacevedo@meteodata.cl>
2024-11-04 10:49:34 -0300

    Función reward actualizada

--- P1_Neuron_Simulation/src/gflownet_functions.py
+++ P1_Neuron_Simulation/src/gflownet_functions.py
@@ -1,67 +1,82 @@
 import torch
+from torch import (
+    exp,
+    eye,
+    nn, 
+    Tensor, 
+    tensor, 
+    zeros_like
+)
 
 
-# def reward_function(original: torch.Tensor, generated: torch.Tensor) -> torch.Tensor:
-#     diff = torch.nn.functional.mse_loss(generated, original)
+# def reward_function(original: Tensor, generated: Tensor) -> Tensor:
+#     diff = nn.functional.mse_loss(generated, original)
 #     return 1 / (diff + 1e-3)
 
 
-def reward_function(original: torch.Tensor, generated: torch.Tensor, matrix: torch.Tensor) -> torch.Tensor:
+def reward_function(original: Tensor, generated: Tensor, matrix: Tensor) -> Tensor:
     if (matrix.sum(dim=0) != 1).any() or (matrix.sum(dim=1) != 1).any():
-        return torch.tensor(0.)
-    diff = torch.nn.functional.mse_loss(generated, original)
+        return tensor(0.)
+    diff = nn.functional.mse_loss(generated, original)
     return 1 / (diff + 1e-3)
 
 
-# def reward_function(original: torch.Tensor, generated: torch.Tensor) -> torch.Tensor:
+# def reward_function(original: Tensor, generated: Tensor) -> Tensor:
 #     if (generated.sum(dim=0) != 1).any() or (generated.sum(dim=1) != 1).any():
-#         return torch.tensor(0.)
-#     return torch.tensor(1.)
+#         return tensor(0.)
+#     return tensor(1.)
+
+# def reward_function(original: Tensor, generated: Tensor, matrix: Tensor) -> Tensor:
+#     # si la matriz tiene algún elemento en la diagonal, está mal
+#     if torch.diag(matrix).sum() > 0:
+#         return tensor(0., device=matrix.device)
+#     diff = nn.functional.mse_loss(generated, original)
+#     return 1 / (diff + 1e-3)
 
 
-def column_reward(matrix: torch.Tensor, std: float = 1.0) -> torch.Tensor:
+def column_reward(matrix: Tensor, std: float = 1.0) -> Tensor:
     n_neurons = len(matrix)
     n_neurons_connected = matrix.sum(dim=0)
-    reward = torch.exp(-(n_neurons_connected - 0.5 * n_neurons) / 2 * (std * std))
+    reward = exp(-(n_neurons_connected - 0.5 * n_neurons) / 2 * (std * std))
     return reward.sum()
 
 
-def get_parents_flow_continuous(state: torch.Tensor, step: int) -> torch.Tensor:
+def get_parents_flow_continuous(state: Tensor, step: int) -> Tensor:
     return state[step].sum()
 
 
-def get_all_binary_matrices(n_spaces: int, flow_function: torch.nn.Module) -> tuple[list[torch.Tensor], list[torch.Tensor]]:
+def get_all_binary_matrices(n_spaces: int, flow_function: nn.Module) -> tuple[list[Tensor], list[Tensor]]:
     all_matrices = []
     all_flow = []
     for k in range(2**n_spaces):
         number = [int(i) for i in bin(k)[2:]]
         if (n := len(number)) < n_spaces:
             number = [0] * (n_spaces - n) + number
-        matrix = torch.tensor(number, dtype=torch.float, device=flow_function.device)
+        matrix = tensor(number, dtype=torch.float, device=flow_function.device)
         all_matrices.append(matrix)
         all_flow.append(flow_function(matrix))
     return all_matrices, all_flow
 
 
-def matrix_to_id(matrix: torch.Tensor) -> int:
+def matrix_to_id(matrix: Tensor) -> int:
     powers = reversed(range(len(matrix)))
-    powers = torch.tensor(list(powers), dtype=torch.float, device=matrix.device)
+    powers = tensor(list(powers), dtype=float, device=matrix.device)
     return int((matrix * 2**powers).sum().item())
 
 
 def get_parents_flow_binary(
-        state: torch.Tensor, 
-        flow_function: torch.nn.Module, 
-        flows: list[torch.Tensor] | None = None
+        state: Tensor, 
+        flow_function: nn.Module, 
+        flows: list[Tensor] | None = None
     ):
     if flows is None:
-        flows, _ = get_all_binary_matrices(len(state), flow_function)
+        _, flows = get_all_binary_matrices(len(state), flow_function)
     
-    if (state == torch.zeros_like(state)).all():
-        return torch.tensor(0.)
+    if (state == zeros_like(state)).all():
+        return tensor(0.)
     
     indexes = state.nonzero(as_tuple=True)[0]
-    state_flow = torch.tensor(0.)
+    state_flow = tensor(0.)
     for i in indexes:
         parent = state.clone()
         parent[i] = 0
@@ -74,13 +89,13 @@ def get_parents_flow_binary(
 
 
 
-# def get_parents_flow_binary(state: torch.Tensor, flow_function: torch.nn.Module) -> torch.Tensor:
+# def get_parents_flow_binary(state: Tensor, flow_function: nn.Module) -> Tensor:
 #     memo = dict()
-#     def get_parents_flow_recursive(state: torch.Tensor, memo: dict, flow_function: torch.nn.Module) -> torch.Tensor:
-#         if (state == torch.zeros_like(state)).all():
-#             return torch.tensor(0.)
+#     def get_parents_flow_recursive(state: Tensor, memo: dict, flow_function: nn.Module) -> Tensor:
+#         if (state == zeros_like(state)).all():
+#             return tensor(0.)
 #         indexes = state.nonzero(as_tuple=True)[0]
-#         state_flow = torch.tensor(0.)
+#         state_flow = tensor(0.)
 #         for i in indexes:
 #             parent = state.clone()
 #             parent[i] = 0
